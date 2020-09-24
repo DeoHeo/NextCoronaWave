79d81bbfae1509920084954466bc2aac89705c65
DeoHeo <nschmidt95@gmx.de>
2020-09-24 11:48:59 +0200

    add function angrenzende Landeskreise

--- NextCoronaWave.ps1
+++ NextCoronaWave.ps1
@@ -16,8 +16,10 @@
     .NOTES
     Author: Deo_Heo
     Last Updated: 13.08.2020
-    Version: 0.1
-  
+    Version: 0.1 Initial Version
+             1.0 Einmonatigen Test
+             2.0 Erweiterung Angrenzende Landkreise
+
     Requires:
     PowerShell
 
@@ -29,6 +31,9 @@
 #OBJECTID choose under the following web page for the appropriate county:
 #https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0?geometry=-26.087%2C46.269%2C46.994%2C55.886
 $objectid = 357
+$objectid2 = 360
+$objectid3 = 358
+$objectid4 = 361
 #set the warn value
 $warn = 40
 #set the crit value(should be left at 50)
@@ -49,9 +54,15 @@ Function GetContent {
     try{
     #API
     $site = Invoke-WebRequest "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=OBJECTID=%27$objectid%27&outFields=OBJECTID,BL,county,cases7_per_100k&returnGeometry=false&f=json" -UseBasicParsing
+    $site2 = Invoke-WebRequest "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=OBJECTID=%27$objectid2%27&outFields=OBJECTID,BL,county,cases7_per_100k&returnGeometry=false&f=json" -UseBasicParsing
+    $site3 = Invoke-WebRequest "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=OBJECTID=%27$objectid3%27&outFields=OBJECTID,BL,county,cases7_per_100k&returnGeometry=false&f=json" -UseBasicParsing
+    $site4 = Invoke-WebRequest "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=OBJECTID=%27$objectid4%27&outFields=OBJECTID,BL,county,cases7_per_100k&returnGeometry=false&f=json" -UseBasicParsing
     IF ($site.StatusDescription -eq "OK"){
         $objoutput = New-Object -TypeName PSObject -Property @{
             value = $site.Content | ConvertFrom-Json
+            value2 = $site2.Content | ConvertFrom-Json
+            value3 = $site3.Content | ConvertFrom-Json
+            value4 = $site4.Content | ConvertFrom-Json
             status = $true
             }
         }
@@ -78,27 +89,45 @@ $objoutput = GetContent -objectid $objectid
 
 If ($objoutput.status){
     #attribute call convert in one variable, that it need for use in the SWITCH Output
-    $lankreis = $objoutput.value.features.attributes.county
+    $lankreis = $objoutput.value.features.attributes.county -replace ' ','_'
+    $lankreis2 = $objoutput.value2.features.attributes.county -replace ' ','_' -replace 'ß','ss'
+    $lankreis3 = $objoutput.value3.features.attributes.county -replace ' ','_'
+    $lankreis4 = $objoutput.value4.features.attributes.county -replace ' ','_' -replace 'ä','ae'
     $bundesland = $objoutput.value.features.attributes.BL
     $var = $objoutput.value.features.attributes.cases7_per_100k
+    $var2 = $objoutput.value2.features.attributes.cases7_per_100k
+    $var3 = $objoutput.value3.features.attributes.cases7_per_100k
+    $var4 = $objoutput.value4.features.attributes.cases7_per_100k
 
     IF ($ger){
         #convert the value cases7_per_100k in the germany format
         $varcount = $var -replace ',','.'
+        $varcount2 = $var2 -replace ',','.'
+        $varcount3 = $var3 -replace ',','.'
+        $varcount4 = $var4 -replace ',','.'
         $var = „{0:N2}“ –f $var
+        $var2 = „{0:N2}“ –f $var2
+        $var3 = „{0:N2}“ –f $var3
+        $var4 = „{0:N2}“ –f $var4
         }
     ELSE{
         #convert the value cases7_per_100k in the english format
         $varcount = $var -replace ',','.'
+        $varcount2 = $var2 -replace ',','.'
+        $varcount3 = $var3 -replace ',','.'
+        $varcount4 = $var4 -replace ',','.'
         $var = „{0:N2}“ –f $var -replace ',','.'
+        $var2 = „{0:N2}“ –f $var2 -replace ',','.'
+        $var3 = „{0:N2}“ –f $var3 -replace ',','.'
+        $var4 = „{0:N2}“ –f $var4 -replace ',','.'
 
         }
 
     #choice the state
-    IF ($var -ge $warn){
+    IF ($var -ge $warn -or $var2 -ge $warn -or $var3 -ge $warn -or $var4 -ge $warn){
         $choice = 1
         }
-    IF ($var -ge $crit){
+    IF ($var -ge $crit -or $var2 -ge $crit -or $var3 -ge $crit -or $var4 -ge $crit){
         $choice = 2
         }
     ELSE{
@@ -113,8 +142,8 @@ ELSE{
 
 
 SWITCH ($choice){
-    0 {"0 $servicename count=$varcount;$warn;$crit;;  OK - $var neue Faelle in den letzten 7 Tagen pro 100.000 Einwohner im $lankreis in $bundesland. Wenn der Schwellwert $crit uebersteigt, gibt es neue Restriktionen vom Landkreis $lankreis."}            
-    1 {"1 $servicename count=$varcount;$warn;$crit;;  WARN - $var neue Faelle in den letzten 7 Tagen pro 100.000 Einwohner im $lankreis in $bundesland. Wenn der Schwellwert $crit uebersteigt, gibt es neue Restriktionen vom Landkreis $lankreis."}            
-    2 {"2 $servicename count=$varcount;$warn;$crit;;  CRIT - $var neue Faelle in den letzten 7 Tagen pro 100.000 Einwohner im $lankreis in $bundesland. Es wird neue Restriktionen geben vom Landkreis $lankreis."}
+    0 {"0 $servicename $lankreis=$varcount;$warn;$crit;;|Angrenzender_$lankreis2=$varcount2;$warn;$crit;;|Angrenzender_$lankreis3=$varcount3;$warn;$crit;;|Angrenzender_$lankreis4=$varcount4;$warn;$crit;;  OK - $var neue Faelle in den letzten 7 Tagen pro 100.000 Einwohner im $lankreis. Wenn der Schwellwert $crit uebersteigt, gibt es neue Restriktionen vom Bundesland $bundesland."}            
+    1 {"1 $servicename $lankreis=$varcount;$warn;$crit;;|Angrenzender_$lankreis2=$varcount2;$warn;$crit;;|Angrenzender_$lankreis3=$varcount3;$warn;$crit;;|Angrenzender_$lankreis4=$varcount4;$warn;$crit;;  WARN - $var neue Faelle in den letzten 7 Tagen pro 100.000 Einwohner im $lankreis oder in einem angrenzenden Landkreis. Wenn der Schwellwert $crit uebersteigt, gibt es neue Restriktionen vom Bundesland $bundesland."}            
+    2 {"2 $servicename $lankreis=$varcount;$warn;$crit;;|Angrenzender_$lankreis2=$varcount2;$warn;$crit;;|Angrenzender_$lankreis3=$varcount3;$warn;$crit;;|Angrenzender_$lankreis4=$varcount4;$warn;$crit;;  CRIT - $var neue Faelle in den letzten 7 Tagen pro 100.000 Einwohner im $lankreis oder in einem angrenzenden Landkreis. Es wird neue Restriktionen vom Bundesland $bundesland geben."}
     3 {"3 $servicename - UNKNOWN - Der Check hat Probleme genauere Details siehe Long output.\n$crashed"}      
 }
